8868948368d1556c9d3e52264f6da689e09711af
hphamdesign <109067024+hphamdesign@users.noreply.github.com>
2022-11-01 19:57:11 -0400

    Update script.js

--- Qualitative_final/script.js
+++ Qualitative_final/script.js
@@ -7,9 +7,20 @@ var drawGraph = d3.csv('https://raw.githubusercontent.com/hphamdesign/Data-Visua
 	.scaleOrdinal()
 	.domain(["Prohibited","21","20","19","18","17","16","14","None"])
 	.range(["#cddc39","#009688","#03a9f4","#3f51b5","#673ab7","#9c27b0","#e81e63","#ff7621","#ffffff"]);
+	
+	// Add a scale for rotation angle
+	let gridAngles = d3
+	.scaleLinear()
+	.domain([0.3,21.2])
+	.range([0,90])
+
+	let gridTransform = d3
+	.scaleLinear()
+	.domain([0.3,21.2])
+	.range(["0,0","50,30"])
 
 	const width = innerWidth;
-	const height = 1320;
+	const height = 1400;
 
 	//create an svg with width and height
 	var svg = d3.select('#my_dataviz')
@@ -24,17 +35,17 @@ var drawGraph = d3.csv('https://raw.githubusercontent.com/hphamdesign/Data-Visua
 
 	var bottles = svg.append("defs")
 		.append("g")
-		.attr("id","bottleIcon");
+		.attr("id","bottleIcon")
 
 	bottles
 		.append("path")
 		.attr("d","M62.444 0C56.6391 0 51.6664 4.1552 50.635 9.86781L47 30H55V130L17.3327 173.024C6.15924 185.786 0 202.171 0 219.134V521.204C0 534.346 11.1929 545 25 545H135C148.807 545 160 534.346 160 521.204V219.134C160 202.171 153.841 185.786 142.667 173.024L105 130V30H112L108.365 9.86781C107.334 4.15521 102.361 0 96.556 0H62.444Z")
-		.attr("transform", "translate(0,0) scale(.175)");
+		.attr("transform", "translate(0,0) scale(.16)");
 		
 
 	//end path definition
 
-	//10 rows and 10 columns 
+	//11 rows and 23 columns 
 	var numRows = 10;
 	var numCols = 24;
 
@@ -57,15 +68,12 @@ var drawGraph = d3.csv('https://raw.githubusercontent.com/hphamdesign/Data-Visua
 	container.selectAll("circle")
 			.data(grid)
 			.enter().append("circle")
-			.attr('class','thumbnail')
+			// .attr('class','thumbnail')
 			.attr("id", function(d){return "id"+d;})
 			.attr('cx', function(d){return x(d%numCols);})
 			.attr('cy', function(d){return y(Math.floor(d/numCols));})
 			.attr('r', 4)
 			.attr('visibility', 'hidden')
-			
-			// .style('fill', 'white')
-			// .style('stroke', 'black');
 
 	let circles = document.querySelectorAll("circle");
 		console.log(circles);
@@ -77,7 +85,7 @@ var drawGraph = d3.csv('https://raw.githubusercontent.com/hphamdesign/Data-Visua
 			];
 		}
 			console.log(list);
-	
+
 	// Create a div for mouseover effect
 	var div = d3.select("body").append("div")
 		.attr("class", "tooltip")
@@ -88,10 +96,22 @@ var drawGraph = d3.csv('https://raw.githubusercontent.com/hphamdesign/Data-Visua
 		.enter()
 		.append("use")
 		.attr("xlink:href", "#bottleIcon")
-		.attr("id","eachBottle")
+		.attr("class","thumbnail")
+		.attr("id", function (d) {return d.LegalAge;})
 		.attr("x", function (d,i) {return list[i][0];})
 		.attr("y", function (d,i) {return list[i][1];})
 		.style("fill", function (d) {return myColor(d.LegalAge);})
+		.attr("transform", function (d) {
+			var xRot = d3.select(this).attr("x");
+			var yRot = d3.select(this).attr("y");
+			function rotation() {
+				return gridAngles(d.AUD);
+			}
+			function transform(){
+				return gridTransform(d.AUD);
+			}
+		return `translate(${transform()})rotate(${rotation()}, ${xRot},  ${yRot /*ES6 template literal to set x and y rotation points*/})` 
+		})
 		.style("stroke", "black")
 		.on('mouseover', function (event, d) {
 			d3.select(this).transition()
@@ -114,11 +134,6 @@ var drawGraph = d3.csv('https://raw.githubusercontent.com/hphamdesign/Data-Visua
 				 .duration('50')
 				 .style("opacity", 0);
 	   });
-	
-
-	// let grabBottles = document.querySelectorAll(`[id^="eachBottle"]`).style.transform="rotate(25deg)";
-	// console.log(grabBottles);
-	
-	
 })
+
 
